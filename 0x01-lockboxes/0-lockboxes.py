812c8b374f5cb89fa6dbff0ff4de912fda2eb20d
Yohannes251 <yohannes.z251@gmail.com>
2022-04-17 20:15:29 +0000

    Created readme and task 1 file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Contains a function that implements lockboxes algorithm
+"""
+
+def canUnlockAll(boxes):
+    """Returns True if all boxes can be opened False otherwise"""
+    opened = [0]
+    box = boxes[0]
+    while True:
+        for j in range(len(box)):
+            if not box[j] in opened:
+                opened.append(box[j])
+                opened_new = 1
+        if j == len(box) - 1:
+            new_box = []
+            for idx in opened:
+                new_box += boxes[idx]
+            box = new_box
+        if opened_new == 0:
+            break
+        opened_new = 0
+    return True if len(opened) == len(boxes) else False
