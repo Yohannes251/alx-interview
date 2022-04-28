30864b50d42b1a220d858d5169d5f9163e669ed4
Yohannes251 <yohannes.z251@gmail.com>
2022-04-29 01:29:02 +0300

    Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+    This script reads stdin line by line and computes metrics
+"""
+
+import sys
+code = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+sum = 0
+
+
+def print_stats():
+    """Prints stats about log"""
+    global sum
+
+    print('File size: {}'.format(sum))
+    stcdor = sorted(code.keys())
+    for each in stcdor:
+        if code[each] > 0:
+            print('{}: {}'.format(each, code[each]))
+
+
+if __name__ == "__main__":
+    cnt = 0
+    try:
+        """ Iterate the standard input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in code:
+                    code[fact[-2]] += 1
+                """ If there is a length """
+                sum += int(fact[-1])
+            except Exception:
+                pass
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                print_stats()
+                cnt = 0
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    else:
+        print_stats()
