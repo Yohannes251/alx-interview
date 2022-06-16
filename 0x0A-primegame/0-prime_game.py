632510d19cf26390e717f49e966aba788f0e7cc6
Yohannes251 <yohannes.z251@gmail.com>
2022-06-16 22:48:52 +0000

    Prime game algo

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    This module contains isWinner function
+    which implements prime game algorithm
+"""
+
+
+def isWinner(x, nums):
+    """Returns winner of the game"""
+
+    is_prime = 0
+    is_not_Prime = 0
+
+    if nums and x > 0:
+        for num in nums:
+            if (num > 0):
+                if(num % 2 == 0):
+                    is_prime += 1
+                else:
+                    is_not_Prime += 1
+        if is_prime >= is_not_Prime:
+            return "Maria"
+        else:
+            return "Ben"
+    else:
+        return None
